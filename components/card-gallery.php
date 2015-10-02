151989d2e59d1c8eb1ea23bae49eeaf71f21e470
m-e-h <info@martyhelmick.com>
2015-10-02 11:56:16 -0400

    background and text color

--- components/card-gallery.php
+++ components/card-gallery.php
@@ -6,13 +6,11 @@
  */
 
 ?>
-<section class="u-flexed-start mdl-cell mdl-card mdl-card-image mdl-shadow--2dp">
-<div class="u-bg-frost-3 u-text-white u-color-inherit">
+<section class="u-bg-frost-3 u-flexed-start mdl-cell mdl-card mdl-card-image mdl-shadow--2dp">
 
         <div <?php hybrid_attr('entry-summary'); ?>>
             <?php tha_entry_content_before(); ?>
             <?php the_content(); ?>
             <?php tha_entry_content_after(); ?>
         </div>
-</div>
 </section>
