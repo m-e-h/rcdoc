b506e93632cf9ad49a7e59a51c531de9e20a5cfb
m-e-h <info@martyhelmick.com>
2015-10-02 11:59:50 -0400

    full width gallery

--- components/card-gallery.php
+++ components/card-gallery.php
@@ -6,9 +6,9 @@
  */
 
 ?>
-<section class="u-bg-frost-3 u-flexed-start mdl-cell mdl-card mdl-card-image mdl-shadow--2dp">
+<section class="u-bg-frost-4 u-flexed-start mdl-cell mdl-card mdl-card-image mdl-shadow--2dp">
 
-        <div <?php hybrid_attr('entry-summary'); ?>>
+        <div class="mdl-card__supporting-text u-1/1">
             <?php tha_entry_content_before(); ?>
             <?php the_content(); ?>
             <?php tha_entry_content_after(); ?>
