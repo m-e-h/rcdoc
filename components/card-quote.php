5e7a438ef8e716738be635e457d1347e4c883e75
Marty Helmick <info@martyhelmick.com>
2015-10-04 21:14:47 -0400

    Update card-quote.php

--- components/card-quote.php
+++ components/card-quote.php
@@ -6,7 +6,7 @@
  */
 ?>
 <section class="u-bg-tint-3 u-text-white u-color-inherit u-1/1 mdl-cell mdl-card mdl-shadow--2dp">
-  <div class="mdl-card__title mdl-card--expand">
+  <div class="mdl-card__title blockquote mdl-card--expand">
           <?php tha_entry_content_before(); ?>
           <?php the_content(); ?>
           <?php tha_entry_content_after(); ?>
