bc05896002c5d8f15522e00ac5443eacb42927b2
Marty Helmick <info@martyhelmick.com>
2015-10-04 20:50:25 -0400

    remove hybrid wrapper div

--- components/card-quote.php
+++ components/card-quote.php
@@ -7,10 +7,8 @@
 ?>
 <section class="u-bg-tint-3 u-text-white u-color-inherit u-1/1 mdl-cell mdl-card mdl-shadow--2dp">
   <div class="mdl-card__title mdl-card--expand">
-      <div <?php hybrid_attr('entry-content'); ?>>
           <?php tha_entry_content_before(); ?>
           <?php the_content(); ?>
           <?php tha_entry_content_after(); ?>
-      </div>
   </div>
 </section>
