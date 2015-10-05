068650ca655b3d1cc884c1d61c6d4e9434886d57
Marty Helmick <info@martyhelmick.com>
2015-10-04 20:38:07 -0400

    Create card-quote.php

---
+++ components/card-quote.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Quote post format template part.
+ *
+ * @package abraham
+ */
+?>
+<section class="u-bg-tint-3 u-text-white u-color-inherit u-1/1 mdl-cell mdl-card mdl-shadow--2dp">
+  <div class="mdl-card__title mdl-card--expand">
+      <div <?php hybrid_attr('entry-content'); ?>>
+          <?php tha_entry_content_before(); ?>
+          <?php the_content(); ?>
+          <?php tha_entry_content_after(); ?>
+      </div>
+  </div>
+</section>
